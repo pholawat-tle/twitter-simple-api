1737b2af27ac19b520bba7025c8392aa2a57cd7a
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-07-06 16:08:18 +0700

    Implement Logging Middleware
    Implement a Logging Middleware function inside transportHTTP package

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -21,14 +21,27 @@ func NewHandler() *Handler {
 	return &Handler{}
 }
 
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		log.WithFields(
+			log.Fields{
+				"Method": r.Method,
+				"Path":   r.URL.Path,
+			},
+		).Info("Request Received")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func (h *Handler) SetUpRoutes() {
 	log.Info("Setting up routes")
 	h.Router = mux.NewRouter()
+	h.Router.Use(LogMiddleware)
 
-	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		if err := json.NewEncoder(w).Encode(Response{Message: "Healthy"}); err != nil {
+	h.Router.HandleFunc("/api/health", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		if err := json.NewEncoder(rw).Encode(Response{Message: "Healthy"}); err != nil {
 			log.Panic(err)
 		}
 	})
